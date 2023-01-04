70bdb03bf42abda00d19edd22c0352a5e5eba1de
arthur <arthur.shekinarfoxy@gmail.com>
2023-01-04 12:09:26 -0300

    feat: session and cookie

--- ruby_modulo3/crypto_wallet/app/controllers/welcome_controller.rb
+++ ruby_modulo3/crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails (isto e um bicoito)"
+    session[:curso] = "Curso de Ruby on Rails (isto e uma sessão)"
     @nome = params[:name]
   end
 end
