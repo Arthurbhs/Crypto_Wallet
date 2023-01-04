4d516055d7b4f8e35046cc07ac1031edc2a85e27
arthur <arthur.shekinarfoxy@gmail.com>
2023-01-04 11:50:43 -0300

    feat: starting translation process

--- ruby_modulo3/crypto_wallet/app/helpers/application_helper.rb
+++ ruby_modulo3/crypto_wallet/app/helpers/application_helper.rb
@@ -1,21 +1,4 @@
 module ApplicationHelper
-    def date_br(data_usa)
-        if I18n.locale == :"pt-BR"
-        data_usa.strftime("%d/%m/%Y")
-        else
-            data_usa.strftime("%m-%d-%Y")
-    end
-end
-
-    def locale(locale)
-       
-        if I18n.locale == :en
-            "English (USA)"
-
-        else
-            "Portugues Brasil"
-        end
-    end
 
     def rails_ambience_br
         if Rails.env.development?
